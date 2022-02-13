b87d12316ed6acbc9457fa81c40958dccc8fb88b
Minyoung-Jang <myimp9721@gmail.com>
2022-02-13 22:52:46 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
-      <Router basename={process.env.PUBLIC_URL}>
-        <App />
-      </Router>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
