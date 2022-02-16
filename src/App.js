025ca34a625c771755d95d9b3a91ed2b70245367
Minyoung-Jang <myimp9721@gmail.com>
2022-02-16 11:14:49 +0900

    디자인 변경 및 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-/**
- * @license
- *
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Main React component that includes the Blockly component.
- * @author samelh@google.com (Sam El-Husseini)
- */
-
 import React from 'react';
 import './App.css';
-import * as Blockly from 'blockly/core';
 
 import BlocklyComponent, { Block, Value, Field, Shadow, Category } from './Blockly';
 
 import BlocklyJS from 'blockly/javascript';
-import BlocklyDart from 'blockly/dart';
-import DartEval from 'blockly/dart_compressed';
-
 import './blocks/customblocks';
 import './generator/generator';
 
@@ -42,12 +15,18 @@ class App extends React.Component {
   }
 
   generateCode = () => {
-    var code = BlocklyJS.workspaceToCode(
+    var codeList = BlocklyJS.workspaceToCode(
       this.simpleWorkspace.current.workspace
     );
-    console.log(code.split("\n\n")[0]);
-    const list = code.split("\n\n");
-    eval(list[0]);
+
+    for (var code of codeList.split("\n\n")) {
+      if (code.includes("//Start\n")) {
+        eval(code);
+      }
+    }
+    // var msg = new SpeechSynthesisUtterance();
+    // msg.text = "안녕하세요";
+    // window.speechSynthesis.speak(msg);
   }
 
   render() {
@@ -60,19 +39,29 @@ class App extends React.Component {
           move={{
             scrollbars: true,
             drag: true,
-            wheel: true
+            wheel: true,
+          }}
+          renderer={'zelos'}
+          zoom=
+          {{
+            controls: true,
+            wheel: true,
+            startScale: 1.0,
+            maxScale: 3,
+            minScale: 0.3,
+            scaleSpeed: 1.2,
+            pinch: true
           }}
           initialXml={`
               <xml xmlns="http://www.w3.org/1999/xhtml">
               </xml>
             `}>
           <Category name="시작" colour="#DB6464">
-            <Block type="start_block" 
-            />
+            <Block type="start_block" />
           </Category>
           <Category name="흐름" colour="#6137D6">
             <Block type="wait_block" />
-            <Block type="controls_repeat_ext" colourPrimary="#6137D6">
+            <Block type="controls_repeat_ext">
               <Value name="TIMES">
                 <Shadow type="math_number">
                   <Field name="NUM">10</Field>
@@ -80,48 +69,48 @@ class App extends React.Component {
               </Value>
             </Block>
           </Category>
-          <Category name="자료" colour="300"></Category>
+          <Category name="자료" colour="300">
+            <Block type="text_print">
+              <Value name="TEXT">
+                <Shadow type="text">
+                  <Field name="TEXT">abc</Field>
+                </Shadow>
+              </Value>
+            </Block>
+            <Block type="variables_get" />
+            <Block type="controls_repeat_ext">
+              <Value name="TIMES">
+                <Shadow type="math_number">
+                  <Field name="NUM">10</Field>
+                </Shadow>
+              </Value>
+            </Block>
+            <Block type="controls_ifelse" />
+            <Block type="logic_compare" />
+            <Block type="logic_operation" />
+            <Block type="logic_operation" />
+            <Block type="logic_negate" />
+            <Block type="text_print">
+              <Value name="TEXT">
+                <Shadow type="text">
+                  <Field name="TEXT">abc</Field>
+                </Shadow>
+              </Value>
+            </Block>
+            <Block type="logic_null" disabled="false" />
+            <Block type="logic_ternary" />
+            <Block type="text_charAt">
+              <Value name="VALUE">
+                <Block type="variables_get">
+                  <Field name="VAR">text</Field>
+                </Block>
+              </Value>
+            </Block>
+          </Category>
           <Category name="연산" colour="400"></Category>
           <Category name="변수" colour="330" custom="VARIABLE"></Category>
           <Category name="스마트홈" colour="500"></Category>
-          <Block type="text_print">
-            <Value name="TEXT">
-              <Shadow type="text">
-                <Field name="TEXT">abc</Field>
-              </Shadow>
-            </Value>
-          </Block>
-          <Block type="logic_operation" />
-          <Block type="controls_repeat_ext">
-            <Value name="TIMES">
-              <Shadow type="math_number">
-                <Field name="NUM">10</Field>
-              </Shadow>
-            </Value>
-          </Block>
-          <Block type="controls_ifelse" />
-          <Block type="logic_compare" />
-          <Block type="logic_operation" />
 
-          <Block type="logic_operation" />
-          <Block type="logic_negate" />
-          <Block type="text_print">
-            <Value name="TEXT">
-              <Shadow type="text">
-                <Field name="TEXT">abc</Field>
-              </Shadow>
-            </Value>
-          </Block>
-          <Block type="test_react_field" />
-          <Block type="logic_null" disabled="false" />
-          <Block type="logic_ternary" />
-          <Block type="text_charAt">
-            <Value name="VALUE">
-              <Block type="variables_get">
-                <Field name="VAR">text</Field>
-              </Block>
-            </Value>
-          </Block>
         </BlocklyComponent>
 
       </div>
--- src/Blockly/BlocklyComponent.jsx
+++ src/Blockly/BlocklyComponent.jsx
@@ -1,26 +1,3 @@
-/**
- * @license
- *
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Blockly React Component.
- * @author samelh@google.com (Sam El-Husseini)
- */
-
 import React from 'react';
 import './BlocklyComponent.css';
 
@@ -43,6 +20,36 @@ class BlocklyComponent extends React.Component {
             this.blocklyDiv.current,
             {
                 toolbox: this.toolbox.current,
+                theme: {
+                    'blockStyles': {
+                        "loop_blocks": {
+                            "colourPrimary": "#6137D6",
+                            "colourSecondary": "#AD7BE9",
+                            "colourTertiary": "#CDB6E9"
+                        },
+                        "logic_blocks": {
+                            "colourPrimary": "#01579b",
+                            "colourSecondary": "#64C7FF",
+                            "colourTertiary": "#C5EAFF"
+                        },
+                        "text_blocks": {
+                            "colourPrimary": "#9B012F",
+                            "colourSecondary": "#FF6498",
+                            "colourTertiary": "#FFC5D6"
+                        },
+                        "variable_blocks": {
+                            "colourPrimary": "#9B012F",
+                            "colourSecondary": "#FF6498",
+                            "colourTertiary": "#FFC5D6"
+                        },
+
+                    },
+                    'componentStyles': {
+                        // 'workspaceBackgroundColour': '#41C75E',
+                        // 'toolboxBackgroundColour': '#C80000'
+                    }
+                },
+
                 ...rest
             },
         );
