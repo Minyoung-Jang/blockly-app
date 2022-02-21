a519486cbd5e8e9023f8785dc1faa275b8860507
Minyoung-Jang <myimp9721@gmail.com>
2022-02-21 13:51:41 +0900

    [UPDATE] 블록 추가

--- src/blocks/customblocks.js
+++ src/blocks/customblocks.js
@@ -1,8 +1,5 @@
 import * as Blockly from 'blockly/core';
 
-import '../fields/BlocklyReactField';
-import '../fields/DateField';
-
 let isAble = false;
 
 var startBlock = {
@@ -103,6 +100,28 @@ Blockly.JavaScript['infinite_loop'] = function (block) {
 Blockly.Blocks['infinite_loop'] = {
   init: function () {
     this.jsonInit(infiniteLoopBlock);
+    this.setColour("#6137D6");
+    this.toLocaleString('ko');
+  },
+};
+
+
+var breakBlock = {
+  "type": "break_loop",
+  "message0": "반복 중단하기",
+  "previousStatement": null,
+  "tooltip": "루프 나오기",
+};
+
+Blockly.JavaScript['break_loop'] = function (block) {
+  var code =
+    `break;\n`;
+  return code;
+};
+
+Blockly.Blocks['break_loop'] = {
+  init: function () {
+    this.jsonInit(breakBlock);
     this.setColour("#6137D6");
     this.toLocaleString('ko');
   },
