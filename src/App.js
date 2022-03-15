fd4e4d2a594db199ffdbcacaa0cc05e366990399
Minyoung-Jang <myimp9721@gmail.com>
2022-03-15 17:20:04 +0900

    [UPDATE] 폰트 변경

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
             <div id="subtitle">PROJECT NAME</div>
             <div id="title">인공지능 홈</div>
           </div>
-          <button id="codeButton" onClick={this.generateCode}>&#x25BA;&nbsp;&nbsp;START! </button>
+          <button id="codeButton" onClick={this.generateCode}><span>&#x25BA; </span>START </button>
         </div>
         <BlocklyComponent
           readOnly={false} trashcan={true} media={'media/'}
