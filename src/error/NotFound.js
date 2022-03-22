8ab71d88c1ebb80b4d7a5164ad2ce153e049f91f
Minyoung-Jang <myimp9721@gmail.com>
2022-03-22 10:39:29 +0900

    [UPDATE] 404 페이지 추가

---
+++ src/error/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+    return (
+        <div className="NotFound">
+        <h3>Sorry, ㅊ!</h3>
+      </div>
+    
+    )
+}
+
+export default NotFound;
