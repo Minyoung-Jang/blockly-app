5f768cfc20858cff99dfc09cb67e823383ae988c
Minyoung-Jang <myimp9721@gmail.com>
2022-04-21 12:17:19 +0900

    [UPDATE] 버튼 수정

--- src/blocks/calculate_blocks/compare_block.js
+++ src/blocks/calculate_blocks/compare_block.js
@@ -1,68 +1,51 @@
-import * as Blockly from 'blockly/core';
+import * as Blockly from "blockly/core";
 
 var compareBlock = {
-    "message0": "%1 %2 %3",
-    "args0": [{
-            "type": "field_input",
-            "name": "A",
-            "text": "1",
-            "output": "Number",
-        },
-        {
-            "type": "field_dropdown",
-            "name": "COMPARE",
-            "options": [
-                [
-                    "<",
-                    "<"
-                ],
-                [
-                    ">",
-                    ">"
-                ],
-                [
-                    ">=",
-                    ">="
-                ],
-                [
-                    "<=",
-                    "<="
-                ],
-                [
-                    "==",
-                    "==="
-                ]
-            ]
-        },
-        {
-            "type": "field_input",
-            "name": "B",
-            "text": "1",
-            "output": "Number",
-        }
-    ],
-    "inputsInline": false,
-    "output": "Boolean",
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-};
-
-Blockly.JavaScript['compare_block'] = function(block) {
-    var a = block.getFieldValue('A');
-    var b = block.getFieldValue('B');
-    var operator = block.getFieldValue('COMPARE');
-    var code =
-        `${a} ${operator} ${b}`;
-    return [code, Blockly.JavaScript.ORDER_ADDITION];
-};
-
-Blockly.Blocks['compare_block'] = {
-    init: function() {
-        this.jsonInit(compareBlock);
-        this.setColour("#7AB964");
-        this.toLocaleString('ko');
+  message0: "%1 %2 %3",
+  args0: [
+    {
+      type: "input_value",
+      name: "A",
+      text: "1",
     },
+    {
+      type: "field_dropdown",
+      name: "COMPARE",
+      options: [
+        ["<", "<"],
+        [">", ">"],
+        [">=", ">="],
+        ["<=", "<="],
+        ["==", "==="],
+      ],
+    },
+    {
+      type: "input_value",
+      name: "B",
+      text: "1",
+    },
+  ],
+  inputsInline: true,
+  output: "Boolean",
+  colour: 230,
+  tooltip: "",
+  helpUrl: "",
+};
+
+Blockly.JavaScript["compare_block"] = function (block) {
+  var a = block.getFieldValue("A");
+  var b = block.getFieldValue("B");
+  var operator = block.getFieldValue("COMPARE");
+  var code = `${a} ${operator} ${b}`;
+  return [code, Blockly.JavaScript.ORDER_ADDITION];
+};
+
+Blockly.Blocks["compare_block"] = {
+  init: function () {
+    this.jsonInit(compareBlock);
+    this.setColour("#7AB964");
+    this.toLocaleString("ko");
+  },
 };
 
 // {"motor" : 180}
